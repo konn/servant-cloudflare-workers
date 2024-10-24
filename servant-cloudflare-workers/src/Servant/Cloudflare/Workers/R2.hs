ce27520fa5c2d8d38f968d51e19eb874946fca64
Hiromi ISHII <konn.jinro@gmail.com>
2024-10-24 23:59:53 +0900

    fix: proper handling of R2 objects

--- servant-cloudflare-workers/src/Servant/Cloudflare/Workers/R2.hs
+++ servant-cloudflare-workers/src/Servant/Cloudflare/Workers/R2.hs
@@ -15,23 +15,28 @@ module Servant.Cloudflare.Workers.R2 (
 ) where
 
 import Control.Concurrent.Async (wait)
+import Control.Monad (forM_)
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Char8 as BS8
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import GHC.Wasm.Object.Builtins
+import qualified GHC.Wasm.Web.Generated.Headers as Headers
 import Network.Cloudflare.Worker.Binding (BindingsClass)
 import qualified Network.Cloudflare.Worker.Binding as Raw
 import Network.Cloudflare.Worker.Binding.R2 (R2, R2Class)
 import qualified Network.Cloudflare.Worker.Binding.R2 as R2
 import qualified Network.Cloudflare.Worker.Request as Req
-import Network.Cloudflare.Worker.Response (WorkerResponseBody (WorkerResponseStream))
-import Network.HTTP.Types (status200)
+import qualified Network.Cloudflare.Worker.Response as Resp
 import Network.HTTP.Types.URI
+import Network.Mime (defaultMimeLookup)
 import Servant.Cloudflare.Workers
-import Servant.Cloudflare.Workers.Internal.Response (PartialResponse (..), RoutingResponse (RouteResponse), toWorkerResponse)
+import Servant.Cloudflare.Workers.Internal.Response (toWorkerResponse)
 import Servant.Cloudflare.Workers.Internal.RoutingApplication
 import Servant.Cloudflare.Workers.Internal.ServerError (responseServerError)
 import Servant.Cloudflare.Workers.Prelude hiding (inject)
+import System.IO.Unsafe (unsafeDupablePerformIO)
+import qualified Wasm.Data.Function.Linear as PL
 
 serveObjectWith :: R2 -> BS.ByteString -> WorkerT e Raw m
 serveObjectWith r2 obj = Tagged \_ _ _ -> do
@@ -41,15 +46,32 @@ serveObjectWith r2 obj = Tagged \_ _ _ -> do
       toWorkerResponse $ responseServerError err500 {errBody = "Failed to fetch R2 object"}
     Just r2body -> do
       src <- R2.getBody r2body
-      toWorkerResponse $
-        RouteResponse
-          PartialResponse
-            { status = status200
-            , headers = []
-            , encodeBody = Nothing
-            , cloudflare = Nothing
-            , body = Just $ WorkerResponseStream src
-            }
+      let etag = R2.getObjectHTTPETag r2body
+          ctype = defaultMimeLookup $ TE.decodeUtf8 $ last $ BS8.split '/' obj
+      hdrs <- Resp.toHeaders mempty
+      R2.writeObjectHttpMetadata r2body hdrs
+      let cacheHdrs =
+            [ ("ETag", etag)
+            , ("Content-Type", ctype)
+            ]
+      forM_ cacheHdrs $ \(k, v) -> do
+        k' <- fromHaskellByteString k
+        v' <- fromHaskellByteString v
+        Headers.js_fun_set_ByteString_ByteString_undefined hdrs k' v'
+      empty <- emptyObject
+      Resp.newResponse' (Just $ inject src) $
+        Just $
+          newDictionary
+            ( setPartialField "statusText" (fromBS "OK")
+                PL.. setPartialField "status" (toJSPrim 200)
+                PL.. setPartialField "headers" (inject hdrs)
+                PL.. setPartialField "encodeBody" (fromBS "automatic")
+                PL.. setPartialField "cf" empty
+            )
+
+fromBS :: BS.ByteString -> JSObject JSByteStringClass
+{-# NOINLINE fromBS #-}
+fromBS = unsafeDupablePerformIO . fromHaskellByteString
 
 serveObject ::
   forall l ->
