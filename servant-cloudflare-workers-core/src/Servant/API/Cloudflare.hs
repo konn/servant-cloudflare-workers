2a07cfd67b732043bfca07dd764a21ef22a466b8
Hiromi ISHII <konn.jinro@gmail.com>
2024-11-10 16:28:07 +0900

    Orphans (unsafe...)

--- servant-cloudflare-workers-core/src/Servant/API/Cloudflare.hs
+++ servant-cloudflare-workers-core/src/Servant/API/Cloudflare.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Servant.API.Cloudflare (
   ReadableStreamBody,
@@ -9,7 +12,13 @@ module Servant.API.Cloudflare (
 import Data.Kind
 import Data.Proxy (Proxy (..))
 import GHC.Generics (Generic)
+import GHC.Wasm.Web.Generated.ReadableStream (ReadableStream)
+import GHC.Wasm.Web.ReadableStream (toReadableStream)
+import Network.Cloudflare.Worker.Response (WorkerResponse, WorkerResponseBody (..))
+import Network.Cloudflare.Worker.Response qualified as Resp
 import Servant.API
+import Streaming.ByteString qualified as Q
+import System.IO.Unsafe (unsafePerformIO)
 
 {- | A request body, that is passed as a @ReadableStream@ on worker side,
 and given by any type on the client side.
@@ -21,3 +30,18 @@ instance (HasLink sub) => HasLink (ReadableStreamBody contentTypes a :> sub) whe
   type MkLink (ReadableStreamBody contentTypes a :> sub) x = MkLink sub x
   toLink toA _ = toLink toA (Proxy @sub)
   {-# INLINE toLink #-}
+
+instance (Accept ct) => MimeUnrender ct ReadableStream where
+  mimeUnrender _ = Right . unsafePerformIO . toReadableStream . Q.fromLazy
+  {-# NOINLINE mimeUnrender #-}
+
+instance (Accept ct) => MimeUnrender ct WorkerResponse where
+  mimeUnrender _ x = Right $ unsafePerformIO do
+    Resp.newResponse
+      Resp.SimpleResponseInit
+        { Resp.statusText = "Dummy"
+        , Resp.status = 200
+        , Resp.headers = mempty
+        , Resp.body = Just $ WorkerResponseLBS x
+        }
+  {-# NOINLINE mimeUnrender #-}
